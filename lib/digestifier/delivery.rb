b3aeee6011c2f0e044c789609ad5a721a66e2cc6
Pat Allan <pat@freelancing-gods.com>
2013-10-08 17:46:08 +1100

    First implementation of the delivery. Spec is green.

--- lib/digestifier/delivery.rb
+++ lib/digestifier/delivery.rb
@@ -1,5 +1,25 @@
 class Digestifier::Delivery
   def self.deliver(digest)
-    #
+    new(digest).deliver
+  end
+
+  def initialize(digest)
+    @digest = digest
+  end
+
+  def deliver
+    recipients.each do |recipient|
+      Digestifier.mailer.digest(recipient, contents).deliver
+    end
+  end
+
+  private
+
+  attr_reader :digest
+
+  delegate :recipients, to: :digest
+
+  def contents
+    digest.contents.call.where(created_at: 1.day.ago..Time.zone.now)
   end
 end
