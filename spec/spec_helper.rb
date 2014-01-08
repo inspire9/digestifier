762c1c6352a79323bc929dbcd8d4b0dda97b8a16
Pat Allan <pat@freelancing-gods.com>
2014-01-08 12:51:08 +1100

    Include RSpec request helpers into acceptance specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,7 @@ Dir["./spec/support/**/*.rb"].each { |file| require file }
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
+
+  config.include RSpec::Rails::RequestExampleGroup, type: :request,
+    example_group: {file_path: /spec\/acceptance/}
 end
