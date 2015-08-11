7889ce39ec30efa9a15ac72a2e959b362f90b181
Pat Allan <pat@freelancing-gods.com>
2015-08-11 16:07:53 -0500

    Reset column information after changing models.

--- db/migrate/5_add_digest_to_settings.rb
+++ db/migrate/5_add_digest_to_settings.rb
@@ -2,6 +2,7 @@ class AddDigestToSettings < ActiveRecord::Migration
   def up
     add_column :digestifier_settings, :digest, :string
     execute "UPDATE digestifier_settings SET digest = 'digest'"
+    Digestifier::Setting.reset_column_information
 
     change_column :digestifier_settings, :digest, :string, null: false
     add_index :digestifier_settings, :digest
@@ -14,6 +15,7 @@ class AddDigestToSettings < ActiveRecord::Migration
   def down
     remove_index :digestifier_settings, name: 'unique_recipients'
     remove_column :digestifier_settings, :digest
+    Digestifier::Setting.reset_column_information
 
     add_index :digestifier_settings, [:recipient_type, :recipient_id],
       unique: true
